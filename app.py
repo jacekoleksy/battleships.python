63f0d02b97e6971a77c231c62851153ec1ba03b7
Jacek Oleksy <jacekoleksy99@gmail.com>
2021-05-06 23:10:14 +0200

    new file:   __pycache__/battleships.cpython-38.pyc 	new file:   app.py 	modified:   battleships.py

---
+++ app.py
@@ -0,0 +1,119 @@
+import tkinter as tk
+import time
+import random
+import battleships as bs
+
+
+class Application(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+
+        self.master.geometry("1000x500")
+        self.master.resizable(0, 0)
+        self.master.title("Battleships")
+
+        self.frame_menu = tk.Frame(self, bg="black", width=200, height=100, padx=20, pady=40)
+        self.frame_menu.grid(row=0, column=0)
+
+        self.frame_player1_text = tk.Frame(self, bg="orange", width=400, height=100)
+        self.frame_player1_text.grid(row=0, column=1)
+        self.frame_player1_text.columnconfigure(0, weight=1)
+
+        self.frame_player2_text = tk.Frame(self, bg="pink", width=400, height=100)
+        self.frame_player2_text.grid(row=0, column=2)
+
+        self.frame_ships = tk.Frame(self, bg="yellow", width=200, height=400, padx=50, pady=50)
+        self.frame_ships.grid(row=1, column=0)
+
+        self.frame_player1_board = tk.Frame(self, bg="green", width=400, height=400)
+        self.frame_player1_board.grid(row=1, column=1)
+
+        self.frame_player2_board = tk.Frame(self, bg="blue", width=400, height=400)
+        self.frame_player2_board.grid(row=1, column=2)
+
+        for frame in [self.frame_menu, self.frame_ships, self.frame_player1_board, self.frame_player2_board, self.frame_player1_text, self.frame_player2_text]:
+            frame.grid_propagate(0)
+            frame.grid(sticky="nswe")
+
+        self.create_widgets()
+
+        self.ship_direction = bs.Direction.NORTH
+        self.ship_length = 4
+
+
+    def create_widgets(self):
+        self.start = tk.Button(self.frame_menu, text="Start game", command=self.start)
+        self.start.pack(side="left")
+
+        self.quit = tk.Button(self.frame_menu, text="Quit", fg="red", command=self.master.destroy)
+        self.quit.pack(side="right")
+
+        self.text1 = tk.Label(self.frame_player1_text, text="Welcome to Battleships", fg="red")
+        self.text1.grid(row=0, column=0)
+        self.text1.grid(ipady=10)
+
+        self.text2 = tk.Label(self.frame_player2_text, text="Created by: Jacek Oleksy PK 2021", fg="red")
+        self.text2.pack(side="top")
+        self.text2.place(rely=0.4, relx=0.28)
+
+    def start(self):
+        self.reset = tk.Button(self.frame_menu, text="Reset", fg="blue", command=self.destroy)
+        self.reset.pack(side="left")
+        self.reset.place(relx=0.49)
+        self.draw_Player_Board(10)
+        self.game = bs.Game()
+
+        self.ship_type_buttons = []
+        for i in range(len(self.game.player_board.available_ship_list)):
+            self.ship_type_buttons.append(tk.Button(self.frame_ships, text=str(i+1) + '-field ship', command=lambda i=i: self.change_ship_length(i+1)))
+            self.ship_type_buttons[-1].grid(row=i, column=1)
+
+        self.direction_buttons = []
+        for i in bs.Direction:
+            self.direction_buttons.append(tk.Button(self.frame_ships, text='Direction'+str(i.name), command=lambda i=i: self.change_direction(i)))
+            self.direction_buttons[-1].grid(row=4+i.value, column=1)
+
+        print('\n'.join(str(p) for p in self.game.ai_board.board))
+        print('\n')
+        print('\n'.join(str(p) for p in self.game.player_board.board))
+
+    def draw_Player_Board(self, size):
+        self.fields_buttons = []
+        for num, i in enumerate(['A','B','C','D','E','F','G','H','I','J']):
+            letter = tk.Label(self.frame_player1_board, text=i, height=2, width=4)
+            letter.grid(row=0, column=num+1)
+        for i in range(10):
+            number = tk.Label(self.frame_player1_board, text=str(i), height=2, width=4)
+            number.grid(row=i+1, column=0)
+        for i in range(size):
+            for j in range(size):
+                self.fields_buttons.append(tk.Button(self.frame_player1_board, text='['+str(i)+' '+str(j)+']', fg="blue", height=1, width=3, command=lambda i=i, j=j: self.add_ship(bs.Ship(i, j, self.ship_direction, self.ship_length))))
+                self.fields_buttons[-1].grid(row=i+1, column=j+1)
+
+    def add_ship(self, ship):
+        if self.game.player_board.add_ship(ship):
+            self.game.player_board.available_ship_list[ship.length-1][1] -= 1
+            for x, y in ship.ship_fields:
+                self.fields_buttons[x*10+y].configure(background='pink')
+            for x, y in ship.ship_overlay_fields_clear:
+                self.fields_buttons[x * 10 + y].configure(background='orange')
+        if sum([y for x, y in self.game.player_board.available_ship_list]) == 0:
+            self.text1.configure(text="Plansza gotowa!")
+            print("Gotowa plansza!")
+
+    def change_ship_length(self, i):
+        self.ship_length = i
+        #self.draw_Player_Board(10)
+        print(self.ship_length)
+
+    def change_direction(self, i):
+        self.ship_direction = i
+        #self.draw_Player_Board(10)
+        print(self.ship_direction.name)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = Application(root)
+    app.mainloop()
--- battleships.py
+++ battleships.py
@@ -1,5 +1,9 @@
 import enum
 import random
+import time
+
+global ship_added
+
 
 class Direction(enum.Enum):
     NORTH = 0
@@ -10,6 +14,7 @@ class Direction(enum.Enum):
     @property
     def next(self):
         return Direction((self.value + 1) % 4)
+
 
 class Ship:
     def __init__(self, xx, yy, direction, length):
@@ -32,49 +37,56 @@ class Ship:
     @property
     def ship_overlay_fields(self):
         if self.direction == Direction.WEST:
-            return [(self.x - 1, self.y - i) for i in range(-1, self.length + 1)] + [(self.x + 1, self.y - i) for i in range(-1, self.length + 1)] + [(self.x, self.y + 1), (self.x, self.y - self.length)]
+            return [(self.x - 1, self.y - i) for i in range(-1, self.length + 1)] + [(self.x + 1, self.y - i) for i in
+                                                                                     range(-1, self.length + 1)] + [
+                       (self.x, self.y + 1), (self.x, self.y - self.length)]
         if self.direction == Direction.SOUTH:
-            return [(self.x + i, self.y - 1) for i in range(-1, self.length + 1)] + [(self.x + i, self.y + 1) for i in range(-1, self.length + 1)] + [(self.x - 1, self.y), (self.x + self.length, self.y)]
+            return [(self.x + i, self.y - 1) for i in range(-1, self.length + 1)] + [(self.x + i, self.y + 1) for i in
+                                                                                     range(-1, self.length + 1)] + [
+                       (self.x - 1, self.y), (self.x + self.length, self.y)]
         if self.direction == Direction.EAST:
-            return [(self.x - 1, self.y + i) for i in range(-1, self.length + 1)] + [(self.x + 1, self.y + i) for i in range(-1, self.length + 1)] + [(self.x, self.y - 1), (self.x, self.y + self.length)]
+            return [(self.x - 1, self.y + i) for i in range(-1, self.length + 1)] + [(self.x + 1, self.y + i) for i in
+                                                                                     range(-1, self.length + 1)] + [
+                       (self.x, self.y - 1), (self.x, self.y + self.length)]
         if self.direction == Direction.NORTH:
-            return [(self.x - i, self.y - 1) for i in range(-1, self.length + 1)] + [(self.x - i, self.y + 1) for i in range(-1, self.length + 1)] + [(self.x + 1, self.y), (self.x - self.length, self.y)]
+            return [(self.x - i, self.y - 1) for i in range(-1, self.length + 1)] + [(self.x - i, self.y + 1) for i in
+                                                                                     range(-1, self.length + 1)] + [
+                       (self.x + 1, self.y), (self.x - self.length, self.y)]
 
     @property
     def ship_overlay_fields_clear(self, size=10):
-        return [row for idx, row in enumerate(self.ship_overlay_fields) if all(n >= 0 and n < size for n in row)]
+        return [row for idx, row in enumerate(self.ship_overlay_fields) if all(0 <= n < size for n in row)]
 
     def rotate(self):
         self.direction = self.direction.next
 
 
 class Board:
-    def __init__(self, size=10, ship_list=[[1, 4], [2, 3], [3, 2], [4, 1]]):
-        self.board = [['o' for i in range(10)] for j in range(10)]
+    def __init__(self, size=10):
+        self.board = [['o' for i in range(size)] for j in range(size)]
         self.size = size
-        self.available_ship_list = ship_list
+        self.available_ship_list = [[1, 4], [2, 3], [3, 2], [4, 1]]
         self.ship_list = []
         self.hits = 0
         self.hits_left = sum([x * y for [x, y] in self.available_ship_list])
 
     def is_available(self, ship):
         for x, y in ship.ship_fields:
-            if x < 0 or y < 0 or x > self.size-1 or y > self.size-1 or self.board[x][y] != 'o':
+            if x < 0 or y < 0 or x > self.size - 1 or y > self.size - 1 or self.board[x][y] != 'o':
                 return False
         return True
 
     def add_ship(self, ship):
+        print(ship.x, ship.y)
         for ship_length, num in self.available_ship_list:
             if ship_length == ship.length and num > 0:
                 if self.is_available(ship):
-                    #self.available_ship_list[ship_length - 1][1] -= 1
                     self.ship_list.append(ship)
                     for x, y in ship.ship_fields:
                         self.board[x][y] = str(ship.length)
                     for x, y in ship.ship_overlay_fields_clear:
                         self.board[x][y] = 'x'
-                    return self.board
-        print("Nie ma miejsca dla tego statku")
+                    return True
         return False
 
     def get_ship(self, x, y):
@@ -94,39 +106,42 @@ class Board:
         if self.get_ship(x, y) is not None:
             self.board[x][y] = '^'
             self.hits += 1
-            print("Gratulacje trafiles! \t Zostalo", self.hits_left-self.hits, "pol do trafienia!")
+            print("Gratulacje trafiles! \t Zostalo", self.hits_left - self.hits, "pol do trafienia!")
         else:
             self.board[x][y] = '.'
-            print("Niestety, spróbuj jeszcze raz! \t Zostalo", self.hits_left-self.hits, "pol do trafienia!")
+            print("Niestety, spróbuj jeszcze raz! \t Zostalo", self.hits_left - self.hits, "pol do trafienia!")
         return True
 
     def gameover(self):
-        if self.hits_left == 0:
+        if self.hits == self.hits_left:
             return True
         return False
 
+
 class RandomBoard(Board):
-    def __init__(self, size=10, ship_list=[[1, 4], [2, 3], [3, 2], [4, 1]]):
-        super().__init__(size, ship_list)
+    def __init__(self, size=10):
+        super().__init__(size)
         for index in range(0, len(self.available_ship_list)):
-            for i in range(0, self.available_ship_list[index-1][1]):
+            for i in range(0, self.available_ship_list[index - 1][1]):
                 ship_added = False
                 while not ship_added:
-                    x = random.randint(0, self.size-1)
-                    y = random.randint(0, self.size-1)
+                    x = random.randint(0, self.size - 1)
+                    y = random.randint(0, self.size - 1)
                     ship_direction = random.choice(list(Direction))
-                    ship = Ship(x, y, ship_direction, self.available_ship_list[index-1][0])
+                    ship = Ship(x, y, ship_direction, self.available_ship_list[index - 1][0])
                     while self.is_available(ship):
                         self.add_ship(ship)
                         ship_added = True
 
+
 class PlayerBoard(Board):
-    def __init__(self, size=10, ship_list=[[1, 4], [2, 3], [3, 2], [4, 1]]):
-        super().__init__(size, ship_list)
+    def __init__(self, size=10):
+        super().__init__(size)
         for index in range(0, len(self.available_ship_list)):
             for i in range(0, self.available_ship_list[index][1]):
                 ship_added = False
                 while not ship_added:
+                    pass
                     print("Wielkosc statku:", self.available_ship_list[index][0])
                     x, y = input('Podaj x i y\t').split()
                     x = int(x)
@@ -137,8 +152,89 @@ class PlayerBoard(Board):
                         ship_added = True
                         print('\n'.join(str(p) for p in self.board))
 
-board1 = RandomBoard()
-print('\n'.join(str(p) for p in board1.board))
-board2 = PlayerBoard()
-print('\n'.join(str(p) for p in board2.board))
 
+class Game:
+    def __init__(self, size=10):
+        self.player_board = Board(size)
+        self.ai_board = RandomBoard(size)
+
+    def player_shoot(self, ai_board):
+        x, y = input("Podaj wspolrzedne strzalu").split()
+        x = int(x)
+        y = int(y)
+        if ai_board.correct_target(x, y):
+            ai_board.shoot(x, y)
+            return True
+        else:
+            return False
+
+    def random_shoot(self, player_board):
+        x, y = -1, -1
+        while not player_board.correct_target(x, y):
+            x = random.randint(0, player_board.size - 1)
+            y = random.randint(0, player_board.size - 1)
+        print(x, y, end=' ')
+        player_board.shoot(x, y)
+
+    def gameover(self):
+        if self.ai_board.gameover():
+            print("Bot wygral")
+            return True
+        elif self.player_board.gameover():
+            print("Ty wygrales")
+            return True
+        return False
+
+    def play(self):
+        if random.randint(0, 1) == 0:
+            if self.player_shoot(self.ai_board):
+                pass
+        time.sleep(0.5)
+        self.random_shoot(self.player_board)
+        while not self.gameover():
+            if self.player_shoot(self.ai_board):
+                time.sleep(0.5)
+                self.random_shoot(self.player_board)
+            # self.random_shoot(self.ai_board)
+            # time.sleep(0.1)
+            # print('\t\t\t', end='')
+            # self.random_shoot(self.player_board)
+            # time.sleep(0.1)
+
+    def reset(self):
+        self.player_board.board.clear()
+        self.player_board = PlayerBoard()
+
+
+class Error(Exception):
+    pass
+
+
+class NoShipThatSizeAvailable(Error):
+    """You cant add a ship with that size"""
+
+    def __init__(self, expression, message):
+        self.expression = expression
+        self.message = message
+
+
+class NoShipThatSizeAvailableException(Error):
+    """You cant add a ship with that size"""
+
+    def __init__(self, expression, message):
+        self.expression = expression
+        self.message = message
+        pass
+
+
+if __name__ == "__main__":
+    ship = Ship(4, 3, Direction.NORTH, 4)
+    game = Game()
+    print('\n'.join(str(p) for p in game.ai_board.board))
+    print('\n')
+    print('\n'.join(str(p) for p in game.player_board.board))
+    game.player_board.add_ship(ship)
+    game.play()
+    print('\n'.join(str(p) for p in game.ai_board.board))
+    print('\n')
+    print('\n'.join(str(p) for p in game.player_board.board))
